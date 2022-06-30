d32df68812881d112e446c9a8734cddadb7a42df
Arnold-O <arnoldoghiator@gmail.com>
2022-06-30 23:56:06 +0100

    all crud func

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -3,39 +3,43 @@ const AppError = require("../middlewares/errorHandler");
 const Job = require("../models/jobsModel");
 
 exports.getAllJobs = asyncWrapper(async (req, res) => {
-
-
-
-    const AllJobs = await Job.find({createdBy: req.user.userId}).sort("createdAt")
+  const AllJobs = await Job.find({ createdBy: req.user.userId }).sort(
+    "createdAt"
+  );
   res.status(200).json({
     AllJobs,
-    count:AllJobs.length
+    count: AllJobs.length,
   });
 });
 
-exports.getJob = asyncWrapper(async (req, res) => {
-
-  const {user:{userId}, params:{id:jobId}} = req
+exports.getJob = asyncWrapper(async (req, res, next) => {
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
 
   const job = await Job.findOne({
-    _id:jobId, createdBy:userId
-  })
+    _id: jobId,
+    createdBy: userId,
+  });
 
-  if(!job){
-    return next(new AppError('no job found with that id'))
+  if (!job) {
+    return next(new AppError("no job found with that id"));
   }
-
-
 
   res.status(200).json({
     message: "success",
-    job
+    job,
   });
 });
 
-
-exports.createJob = asyncWrapper(async (req, res) => {
+exports.createJob = asyncWrapper(async (req, res, next) => {
   const { company, position, status } = req.body;
+
+
+  if(!company || !position){
+    return next(new AppError('please enter company and position',401))
+  }
 
   const job = await Job.create({
     company,
@@ -45,17 +49,42 @@ exports.createJob = asyncWrapper(async (req, res) => {
   });
 
   res.status(200).json({
-    job
+    job,
   });
 });
 
-exports.updateJob = asyncWrapper(async (req, res) => {
-  
+exports.updateJob = asyncWrapper(async (req, res, next) => {
+  const {
+    body:{company, position},
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+ if(company === "" || position===""){
+  return next(new AppError('please provide the company or position field', 400))
+ }
+
+ const jobUpdate = await Job.findByIdAndUpdate({_id:jobId, createdBy:userId}, req.body,{
+  new:true,
+  runValidators:true
+ })
+ if(!jobUpdate){
+  return next(new AppError('Job doesnt exit', 400))
+
+
+ }
   res.status(200).json({
-    message: "success",
+    jobUpdate,
   });
 });
 exports.deleteJobs = asyncWrapper(async (req, res) => {
+  const {
+    body:{company, position},
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  const job = await Job
   res.status(200).json({
     message: "success",
   });
